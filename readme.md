7d05745e4e069539a99bc6efafc15a909a33c686
Tyler Matzelle <69399745+TylerMatzelle@users.noreply.github.com>
2020-09-14 20:00:38 -0700

    Initial commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+# My Typescript Package
+
+---
+
+### Installation Instructions
+
+To use this package, please run:
+
+```
+npm install
+```
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+function helloworld(message: string) {
+    console.log("Hello World!");
+
+    const updatedmessage = { f: 1, b: 2 };
+
+    return 0;
+}
+
+helloworld("Goodbye World");
